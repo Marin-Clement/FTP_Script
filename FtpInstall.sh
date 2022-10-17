e9266cd0a6770d4c3c4b7a1bcb391f0b5f50f2e7
Clément Marin <clementmarin@MacBook-Air-de-Clement.local>
2022-10-17 18:07:55 +0200

    1.04

--- FtpInstall.sh
+++ FtpInstall.sh
@@ -4,7 +4,7 @@
 
 apt update ; apt upgrade ; apt install proftpd ;
 cp /etc/proftpd/proftpd.conf /etc/proftpd/proftpd.conf.back
-cat Ftp_Script/reference.txt > /etc/proftpd/proftpd.conf
+cat FTP_Script/reference.txt > /etc/proftpd/proftpd.conf
 service proftpd restart
 # ip=$(hostname -I | cut -f1 -d' ')
 # echo $ip
