34b204f1844b704d524bb2cd2cfa3388b595ad34
Clément Marin <clementmarin@MacBook-Air-de-Clement.local>
2022-10-17 17:58:53 +0200

    1.02

--- FtpInstall.sh
+++ FtpInstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#git clone https://github.com/remi-vidal-michel/referencecfg.git ; sh FtpInstall.sh
+#git clone https://github.com/remi-vidal-michel/referencecfg.git ; sh Ftp_Script/FtpInstall.sh
 
-# sudo apt update ; sudo apt upgrade ; sudo apt install proftpd ; sudo apt git
-cp /etc/proftpd/proftpd.conf /etc/proftpd/proftpd1.conf
-cat referencecfg/referencecfg.txt > /etc/proftpd/proftpd23.conf
+sudo apt update ; sudo apt upgrade ; sudo apt install proftpd ;
+cp /etc/proftpd/proftpd.conf /etc/proftpd/proftpd.conf.back
+cat Ftp_Script/reference > /etc/proftpd/proftpd.conf
 service proftpd restart
 # ip=$(hostname -I | cut -f1 -d' ')
 # echo $ip
