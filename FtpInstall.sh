a35cf4b850170ea719a01af49f8fb716ecd90ece
Clément Marin <clementmarin@MacBook-Air-de-Clement.local>
2022-10-18 09:52:59 +0200

    1.04

--- FtpInstall.sh
+++ FtpInstall.sh
@@ -1,10 +1,32 @@
-#!/bin/bash
+# ./bin/bash
 
-#git clone https://github.com/Marin-Clement/FTP_Script.git ; sh Ftp_Script/FtpInstall.sh
+#Set user to root for commands
+echo "Password needed to make changes to your machine"
+su /
 
-apt update ; apt upgrade ; apt install proftpd ;
-cp /etc/proftpd/proftpd.conf /etc/proftpd/proftpd.conf.back
-cat FTP_Script/reference.txt > /etc/proftpd/proftpd.conf
+#Installation package
+apt -y install proftpd 
+apt-get update ; apt-get upgrade
+echo "Installation done"
+
+#Starting server
+service proftpd start
+echo "Starting the server"
+cp /etc/proftpd/proftpd.conf /etc/proftpd/proftpd.conf.backup
+echo "A Backup to /etc/proftpd/proftpd.conf.backup has been made"
+
+#Echo server's IP Adress
+hostname -I | cut -f1 -d' '
+echo "This is the server's IP"
+
+#Enabling Anonymous and FTPS
+mkdir -p /etc/proftpd/ssl/
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -out proftpd-rsa.pem -keyout /etc/proftpd/ssl/proftpd-key.pem 
+chmod 440 /etc/proftpd/ssl/proftpd-key.pem
+cat FTPscrit/referencecfg.txt > /etc/proftpd/proftpd23.conf
+
+#Starting operating server
 service proftpd restart
-# ip=$(hostname -I | cut -f1 -d' ')
-# echo $ip
+echo "User Anonymous is now enabled"
+echo "Your FTP is now encrypted (FTPS)"
+echo "To remove all changes to your machine please launch Uninstall.sh"
