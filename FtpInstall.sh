13dbc0a93faa85ced1a3a329367fff866eb47597
Clément Marin <clementmarin@MacBook-Air-de-Clement.local>
2022-10-18 09:57:44 +0200

    1.05

--- FtpInstall.sh
+++ FtpInstall.sh
@@ -1,5 +1,5 @@
 # ./bin/bash
-
+git clone https://github.com/Marin-Clement/FTP_Script.git ; sh FTP_Script/FtpInstall.sh
 #Set user to root for commands
 echo "Password needed to make changes to your machine"
 su /
@@ -23,7 +23,7 @@ echo "This is the server's IP"
 mkdir -p /etc/proftpd/ssl/
 openssl req -x509 -nodes -days 365 -newkey rsa:4096 -out proftpd-rsa.pem -keyout /etc/proftpd/ssl/proftpd-key.pem 
 chmod 440 /etc/proftpd/ssl/proftpd-key.pem
-cat FTPscrit/referencecfg.txt > /etc/proftpd/proftpd23.conf
+cat FTP_Script/reference.txt > /etc/proftpd/proftpd.conf
 
 #Starting operating server
 service proftpd restart
